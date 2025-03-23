e501c6faffb270a8b7b96ba7d18d8813b3f60568
userinsaturn <nicoleechengg@gmail.com>
2025-03-23 22:37:33 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,21 @@
+
+
+<footer class="container-fluid text-center">
+<div class="container">
+<div class="row">
+<div class="col-md-6">
+  <a class="navbar-brand" href="#"><span class="josh">JOSH</span> <span class="orange">WHITKIN</span></a>
+</div>
+
+<div class="col-md-6">
+    <p> Terms and Conditions </p>
+    </div>
+
+</div><!-- row -->
+</div><!-- container -->
+</footer>
+
+</section>
+<?php wp_footer(); ?>
+</body>
+</html>
